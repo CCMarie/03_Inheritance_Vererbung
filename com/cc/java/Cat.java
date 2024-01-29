c31209613e27c436a4b3e3957bf6397f8fc218c1
Maria <sydowmarie4@gmail.com>
2024-01-29 10:59:45 +0100

    SuperCat

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,27 +1,14 @@
 package com.cc.java;
 
-public class Cat {
+public class Cat extends SuperCat{
 
-  private String name;
-  private String furColor;
-  private int age; 
-  private int counter;
-
+  
   public Cat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
+    
   }
 
   public String getStringAttributes(String op) {
-    switch (op) {
-      case "#name":
-        return name;
-      case "#color":
-        return furColor;
-      default:
-        return "ERROR!";
-    }
+    
   }
 
   public String getAge() {
